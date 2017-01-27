516f84e73bcc98f7e9ee70835e9e4c0db6fd51f8
Dénes Zsolt <zsolt.denes@gmail.com>
2017-01-27 14:23:49 +0100

    formatting the code

--- src/com/jcraft/jsch/ChannelSession.java
+++ src/com/jcraft/jsch/ChannelSession.java
@@ -1,56 +1,54 @@
 /* -*-mode:java; c-basic-offset:2; indent-tabs-mode:nil -*- */
 /*
-Copyright (c) 2002-2010 ymnk, JCraft,Inc. All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-  1. Redistributions of source code must retain the above copyright notice,
-     this list of conditions and the following disclaimer.
-
-  2. Redistributions in binary form must reproduce the above copyright 
-     notice, this list of conditions and the following disclaimer in 
-     the documentation and/or other materials provided with the distribution.
-
-  3. The names of the authors may not be used to endorse or promote products
-     derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES,
-INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL JCRAFT,
-INC. OR ANY CONTRIBUTORS TO THIS SOFTWARE BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
-OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Copyright (c) 2002-2010 ymnk, JCraft,Inc. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided with
+ * the distribution.
+ * 
+ * 3. The names of the authors may not be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL JCRAFT, INC. OR ANY CONTRIBUTORS TO THIS SOFTWARE BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 package com.jcraft.jsch;
 
 import java.util.*;
 
-class ChannelSession extends Channel{
-  private static byte[] _session=Util.str2byte("session");
+class ChannelSession extends Channel {
+  private static byte[] _session = Util.str2byte("session");
 
-  protected boolean agent_forwarding=false;
-  protected boolean xforwading=false;
-  protected Hashtable env=null;
+  protected boolean agent_forwarding = false;
+  protected boolean xforwading = false;
+  protected Hashtable env = null;
 
-  protected boolean pty=false;
+  protected boolean pty = false;
 
-  protected String ttype="vt100";
-  protected int tcol=80;
-  protected int trow=24;
-  protected int twp=640;
-  protected int thp=480;
-  protected byte[] terminal_mode=null;
+  protected String ttype = "vt100";
+  protected int tcol = 80;
+  protected int trow = 24;
+  protected int twp = 640;
+  protected int thp = 480;
+  protected byte[] terminal_mode = null;
 
-  ChannelSession(){
+  ChannelSession() {
     super();
-    type=_session;
-    io=new IO();
+    type = _session;
+    io = new IO();
   }
 
   /**
@@ -58,8 +56,8 @@ class ChannelSession extends Channel{
    *
    * @param enable
    */
-  public void setAgentForwarding(boolean enable){ 
-    agent_forwarding=enable;
+  public void setAgentForwarding(boolean enable) {
+    agent_forwarding = enable;
   }
 
   /**
@@ -68,8 +66,8 @@ class ChannelSession extends Channel{
    * @param enable
    * @see RFC4254 6.3.1. Requesting X11 Forwarding
    */
-  public void setXForwarding(boolean enable){
-    xforwading=enable; 
+  public void setXForwarding(boolean enable) {
+    xforwading = enable;
   }
 
   /**
@@ -77,23 +75,21 @@ class ChannelSession extends Channel{
    * @see #setEnv(String, String)
    * @see #setEnv(byte[], byte[])
    */
-  public void setEnv(Hashtable env){ 
-    synchronized(this){
-      this.env=env; 
+  public void setEnv(Hashtable env) {
+    synchronized (this) {
+      this.env = env;
     }
   }
 
   /**
-   * Set the environment variable. 
-   * If <code>name</code> and <code>value</code> are needed to be passed 
-   * to the remote in your faivorite encoding,use 
-   * {@link #setEnv(byte[], byte[])}.
+   * Set the environment variable. If <code>name</code> and <code>value</code> are needed to be
+   * passed to the remote in your faivorite encoding,use {@link #setEnv(byte[], byte[])}.
    *
    * @param name A name for environment variable.
    * @param value A value for environment variable.
    * @see RFC4254 6.4 Environment Variable Passing
    */
-  public void setEnv(String name, String value){
+  public void setEnv(String name, String value) {
     setEnv(Util.str2byte(name), Util.str2byte(value));
   }
 
@@ -105,15 +101,14 @@ class ChannelSession extends Channel{
    * @see #setEnv(String, String)
    * @see RFC4254 6.4 Environment Variable Passing
    */
-  public void setEnv(byte[] name, byte[] value){
-    synchronized(this){
+  public void setEnv(byte[] name, byte[] value) {
+    synchronized (this) {
       getEnv().put(name, value);
     }
   }
 
-  private Hashtable getEnv(){
-    if(env==null)
-      env=new Hashtable();
+  private Hashtable getEnv() {
+    if (env == null) env = new Hashtable();
     return env;
   }
 
@@ -123,8 +118,8 @@ class ChannelSession extends Channel{
    * @param enable
    * @see RFC4254 6.2. Requesting a Pseudo-Terminal
    */
-  public void setPty(boolean enable){ 
-    pty=enable; 
+  public void setPty(boolean enable) {
+    pty = enable;
   }
 
   /**
@@ -132,8 +127,8 @@ class ChannelSession extends Channel{
    * 
    * @param terminal_mode
    */
-  public void setTerminalMode(byte[] terminal_mode){
-    this.terminal_mode=terminal_mode;
+  public void setTerminalMode(byte[] terminal_mode) {
+    this.terminal_mode = terminal_mode;
   }
 
   /**
@@ -145,35 +140,32 @@ class ChannelSession extends Channel{
    * @param hp terminal height, pixels
    * @see RFC4254 6.7. Window Dimension Change Message
    */
-  public void setPtySize(int col, int row, int wp, int hp){
+  public void setPtySize(int col, int row, int wp, int hp) {
     setPtyType(this.ttype, col, row, wp, hp);
-    if(!pty || !isConnected()){
+    if (!pty || !isConnected()) {
       return;
     }
-    try{
-      RequestWindowChange request=new RequestWindowChange();
+    try {
+      RequestWindowChange request = new RequestWindowChange();
       request.setSize(col, row, wp, hp);
       request.request(getSession(), this);
-    }
-    catch(Exception e){
+    } catch (Exception e) {
       //System.err.println("ChannelSessio.setPtySize: "+e);
     }
   }
 
   /**
-   * Set the terminal type.
-   * This method is not effective after Channel#connect().
+   * Set the terminal type. This method is not effective after Channel#connect().
    *
    * @param ttype terminal type(for example, "vt100")
    * @see #setPtyType(String, int, int, int, int)
    */
-  public void setPtyType(String ttype){
+  public void setPtyType(String ttype) {
     setPtyType(ttype, 80, 24, 640, 480);
   }
 
   /**
-   * Set the terminal type.
-   * This method is not effective after Channel#connect().
+   * Set the terminal type. This method is not effective after Channel#connect().
    *
    * @param ttype terminal type(for example, "vt100")
    * @param col terminal width, columns
@@ -181,96 +173,90 @@ class ChannelSession extends Channel{
    * @param wp terminal width, pixels
    * @param hp terminal height, pixels
    */
-  public void setPtyType(String ttype, int col, int row, int wp, int hp){
-    this.ttype=ttype;
-    this.tcol=col;
-    this.trow=row;
-    this.twp=wp;
-    this.thp=hp;
+  public void setPtyType(String ttype, int col, int row, int wp, int hp) {
+    this.ttype = ttype;
+    this.tcol = col;
+    this.trow = row;
+    this.twp = wp;
+    this.thp = hp;
   }
 
-  protected void sendRequests() throws Exception{
-    Session _session=getSession();
+  protected void sendRequests() throws Exception {
+    Session _session = getSession();
     Request request;
-    if(agent_forwarding){
-      request=new RequestAgentForwarding();
+    if (agent_forwarding) {
+      request = new RequestAgentForwarding();
       request.request(_session, this);
     }
 
-    if(xforwading){
-      request=new RequestX11();
+    if (xforwading) {
+      request = new RequestX11();
       request.request(_session, this);
     }
 
-    if(pty){
-      request=new RequestPtyReq();
-      ((RequestPtyReq)request).setTType(ttype);
-      ((RequestPtyReq)request).setTSize(tcol, trow, twp, thp);
-      if(terminal_mode!=null){
-        ((RequestPtyReq)request).setTerminalMode(terminal_mode);
+    if (pty) {
+      request = new RequestPtyReq();
+      ((RequestPtyReq) request).setTType(ttype);
+      ((RequestPtyReq) request).setTSize(tcol, trow, twp, thp);
+      if (terminal_mode != null) {
+        ((RequestPtyReq) request).setTerminalMode(terminal_mode);
       }
       request.request(_session, this);
     }
 
-    if(env!=null){
-      for(Enumeration _env=env.keys(); _env.hasMoreElements();){
-        Object name=_env.nextElement();
-        Object value=env.get(name);
-        request=new RequestEnv();
-        ((RequestEnv)request).setEnv(toByteArray(name), 
-                                     toByteArray(value));
+    if (env != null) {
+      for (Enumeration _env = env.keys(); _env.hasMoreElements();) {
+        Object name = _env.nextElement();
+        Object value = env.get(name);
+        request = new RequestEnv();
+        ((RequestEnv) request).setEnv(toByteArray(name), toByteArray(value));
         request.request(_session, this);
       }
     }
   }
 
-  private byte[] toByteArray(Object o){
-    if(o instanceof String){
-      return Util.str2byte((String)o);
+  private byte[] toByteArray(Object o) {
+    if (o instanceof String) {
+      return Util.str2byte((String) o);
     }
-    return (byte[])o;
+    return (byte[]) o;
   }
 
-  public void run(){
+  public void run() {
     //System.err.println(this+":run >");
 
-    Buffer buf=new Buffer(rmpsize);
-    Packet packet=new Packet(buf);
-    int i=-1;
-    try{
-      while(isConnected() &&
-	    thread!=null && 
-            io!=null && 
-            io.in!=null){
-        i=io.in.read(buf.buffer, 
-                     14,    
-                     buf.buffer.length-14
-                     -32 -20 // padding and mac
-		     );
-	if(i==0)continue;
-	if(i==-1){
-	  eof();
-	  break;
-	}
-	if(close)break;
+    Buffer buf = new Buffer(rmpsize);
+    Packet packet = new Packet(buf);
+    int i = -1;
+    try {
+      while (isConnected() && thread != null && io != null && io.in != null) {
+        i = io.in.read(buf.buffer, 14, buf.buffer.length - 14 - 32 - 20 // padding and mac
+        );
+        if (i == 0) continue;
+        if (i == -1) {
+          eof();
+          break;
+        }
+        if (close) break;
         //System.out.println("write: "+i);
         packet.reset();
-        buf.putByte((byte)Session.SSH_MSG_CHANNEL_DATA);
+        buf.putByte((byte) Session.SSH_MSG_CHANNEL_DATA);
         buf.putInt(recipient);
         buf.putInt(i);
         buf.skip(i);
-	getSession().write(packet, this, i);
+        getSession().write(packet, this, i);
       }
-    }
-    catch(Exception e){
+    } catch (Exception e) {
       //System.err.println("# ChannelExec.run");
       //e.printStackTrace();
     }
-    Thread _thread=thread; 
-    if(_thread!=null){
-      synchronized(_thread){ _thread.notifyAll(); }
+    Thread _thread = thread;
+    if (_thread != null) {
+      synchronized (_thread) {
+        _thread.notifyAll();
+      }
     }
-    thread=null;
+    thread = null;
     //System.err.println(this+":run <");
   }
 }
